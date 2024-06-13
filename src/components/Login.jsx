c33893d94c746fc4b46599d39a14142eadd5d35b
riz2001 <mohammedrizwanps@gmail.com>
2024-06-13 14:43:14 +0530

    complete

---
+++ src/components/Login.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <div>
+        <br></br>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row">
+                    
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">NAME</label>
+                            <input type="text" className="form-control"/>
+                            <br></br>
+                 
+                            
+                          
+
+                        </div>
+
+                          
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label-form className="label"></label-form>
+                        <br></br>
+                     
+                           
+                 
+
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">EMAIL</label>
+                            <input type="text" className="form-control"/>
+                            <br></br>
+                            </div>
+                           
+                        
+
+                     
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">PASSWORD</label>
+                            <input type="text" className="form-control"/>
+                            <br></br>
+                            </div>
+                            <center>
+                          
+                 
+                       
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-success">REGISTER</button>
+                        </div>
+                        <br></br>
+                        </center>
+                      
+                        <center>
+
+                        
+                        <div className="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                         <a href=''>NEW USER</a>
+                        </div>
+                        </center>
+                    
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  )
+}
